fe1d1898738eea981912bd72f11780a061baf461
velina1412 <67910989+velina1412@users.noreply.github.com>
2020-07-16 13:16:32 +0530

    Update text.php

--- text.php
+++ text.php
@@ -1,12 +1,4 @@
-<html>
-<body>
-<form action="text.php" method="post">
-N1: <input type="text" name="n1"><br>
-N2: <input type="text" name="n2"><br>
-<input type="submit" name="check" value="Prime Nos in the range N1 and N2">
-</form>
-</body>
-</html>
+
 <?php
 $n1=$_POST["n1"];
 $n2=$_POST["n2"];
@@ -23,6 +15,17 @@ continue;
 echo " is the prime number in range N1 and N2. ";
 }
 ?>
+<?php
+	$pr_id=$_POST["Id"];
+	$pr_name=$_POST["Name"];
+	$pr_price=$_POST["Price"];
+	$pr_des=$_POST["Description"];
+    $conn=new mysqli('localhost','root','','info');
+	$sql="create table 'product details'(roll INT(20),bname VARCHAR(20)";
+	mysqli_query($conn,$sql);
+	mysqli_close($conn);
+//insert into productdetails(productid,productname,productprice,productdes) values($p_id,$p_name,$p_price,$p_des)	?>
+
 
 
 
